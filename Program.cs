2823cc0f436b760c5905c2908c042ad739a69132
Programação-java <kaue171717@gmail.com>
2025-05-19 23:28:32 -0300

    Adicione todas ás alterações que eu já fiz

--- Program.cs
+++ Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Carro
+{
+    public string Modelo { get; set; }
+    public string Cor { get; set; }
+    public string Marca { get; set; }
+    public string Placa { get; set; }
+    public bool Disponivel { get; private set; }
+
+    public Carro(string modelo, string cor, string marca, string placa)
+    {
+        Modelo = modelo;
+        Cor = cor;
+        Marca = marca;
+        Placa = placa;
+        Disponivel = true;
+    }
+
+    public void Alugar()
+    {
+        Disponivel = false;
+        Console.WriteLine($"🚗 O carro {Modelo} foi alugado.");
+    }
+
+    public void Devolver()
+    {
+        Disponivel = true;
+        Console.WriteLine($"✅ O carro {Modelo} foi devolvido.");
+    }
+}
+
+// Classe Cliente separada
+class Cliente
+{
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public string Telefone { get; set; }
+
+    public Cliente(string nome, string cpf, string telefone)
+    {
+        Nome = nome;
+        CPF = cpf;
+        Telefone = telefone;
+    }
+}
+
+// Classe Locacao separada
+class Locacao
+{
+    public Cliente Cliente { get; set; }
+    public Carro Carro { get; set; }
+    public DateTime DataLocacao { get; set; }
+    public DateTime DataInicio { get; }
+    public DateTime DataFim { get; set; }
+
+    public Locacao(Cliente cliente, Carro carro, DateTime dataInicio)
+    {
+        Cliente = cliente;
+        Carro = carro;
+        DataInicio = dataInicio;
+        DataLocacao = DateTime.Now; // Assumindo que a locação ocorre no momento de criação
+        Carro.Alugar();
+    }
+
+    public void FinalizarLocacao(DateTime dataFim)
+    {
+        DataFim = dataFim;
+        Carro.Devolver();
+        Console.WriteLine($"📅 Locação encerrada. Cliente: {Cliente.Nome}, Carro: {Carro.Modelo}, Período: {DataInicio} a {DataFim}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro carro1 = new Carro("Camaro", "Amarelo", "Chevrolet", "ABC-1234");
+    Cliente cliente1 = new Cliente("Bruno Rodrigues", "123.456.789-00", "(11) 98765-4321");
+        Locacao locacao1 = new Locacao(cliente1, carro1, DateTime.Now);
+
+        // Simulando o final da locação após 3 dias
+        locacao1.FinalizarLocacao(DateTime.Now.AddDays(3));
+    }
+}
--- obj/Debug/net9.0/Projeto-locadora-de-carros.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projeto-locadora-de-carros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-locadora-de-carros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec05415331130ef17e8260e03ff828703ca2838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217568e7d00b4f2d1f41257f40f2f2037b579154")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-locadora-de-carros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-locadora-de-carros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
